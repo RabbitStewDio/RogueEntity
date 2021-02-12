9f977429a26642f2eede58925e855670f2f685d0
Thomas Morgner <git@rabbit-stew-dio.com>
2021-02-12 12:07:22 +0000

    Utility Extension method to simply type comparisons.
    (Note: This is helpful in quick and dirty games, but ideally your
    systems are built around systems with entities marked with role
    based entity traits to pick out matching things. Its better to
    check for a trait/existence of data than to hardcode item names.)

---
+++ src/RogueEntity.Api/ItemTraits/ItemResolverExtensions.cs
@@ -0,0 +1,13 @@
+﻿using EnTTSharp.Entities;
+
+namespace RogueEntity.Api.ItemTraits
+{
+    public static class ItemResolverExtensions
+    {
+        public static bool IsItemType<TEntity>(this IItemResolver<TEntity> r, TEntity k, ItemDeclarationId d)
+            where TEntity : IEntityKey
+        {
+            return r.TryResolve(k, out var id) && id.Id == d;
+        }
+    }
+}
