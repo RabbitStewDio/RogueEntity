b6e2caecaf3b0f1eeb59b61aff7479c46319e974
Thomas Morgner <git@rabbit-stew-dio.com>
2021-02-13 12:09:02 +0000

    Splitting sample projects into separate projects.

---
+++ samples/RogueEntity.Samples.MineSweeper.MonoGame/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace RogueEntity.Samples.MineSweeper.MonoGame
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
